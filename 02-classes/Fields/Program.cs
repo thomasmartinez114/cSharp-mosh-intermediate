5bc2b5905069c297c16fd36efbadc3daf4303adb
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-24 23:22:30 -0500

    init constructors in Fields

---
+++ 02-classes/Fields/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+    }
+
+    public class Order
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
