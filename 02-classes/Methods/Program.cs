b3d307927f4ff0ea19d1a9eb947516f8a5b7447a
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-24 22:53:14 -0500

    moved point again

--- 02-classes/Methods/Program.cs
+++ 02-classes/Methods/Program.cs
@@ -9,6 +9,9 @@ namespace Methods
             var point = new Point(10, 20);
             point.Move(new Point(40, 60));
             Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+            point.Move(100, 200);
+            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
         }
     }
 }
