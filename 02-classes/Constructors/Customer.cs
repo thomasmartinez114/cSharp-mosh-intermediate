893239c75af2e5ffd125083572bfb1fda4427e76
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-24 22:29:40 -0500

    this() and initlaize object

--- 02-classes/Constructors/Customer.cs
+++ 02-classes/Constructors/Customer.cs
@@ -1,18 +1,28 @@
-﻿namespace Constructors
+﻿using System.Collections.Generic;
+
+namespace Constructors
 {
     public class Customer
     {
         public int Id;
         public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+            // always make sure a List is empty initliazied
+        }
 
         public Customer(int id)
+            : this()
         {
             this.Id = id;
         }
 
         public Customer(int id, string name)
+            : this(id)
         {
-            this.Id = id;
             this.Name = name;
         }
     }
--- 02-classes/Constructors/Program.cs
+++ 02-classes/Constructors/Program.cs
@@ -2,11 +2,18 @@
 
 namespace Constructors
 {
+
     class Program
     {
         static void Main(string[] args)
         {
             var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John"
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
         }
