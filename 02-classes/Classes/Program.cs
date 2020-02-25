bb7d04181278cce2c08585b9071df3da3fe32395
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-24 21:36:46 -0500

    using static method

--- 02-classes/Classes/Program.cs
+++ 02-classes/Classes/Program.cs
@@ -10,13 +10,20 @@ namespace Classes
         {
             Console.WriteLine("Hi {0}, I am {1}", to, Name);
         }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.Name = "John";
+            var person = Person.Parse("John");
             person.Introduce("Mosh");
         }
     }
