a61488aa129aaf0c1a2a865e86eaf2f4a678deab
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-24 23:32:10 -0500

    moved to another instance to clean up Program.cs -- count the Orders

--- 02-classes/Fields/Program.cs
+++ 02-classes/Fields/Program.cs
@@ -1,36 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Fields
 {
-    public class Customer
-    {
-        public int Id;
-        public string Name;
-        public List<Order> Orders = new List<Order>();
-
-        public Customer(int id)
-        {
-            this.Id = id;
-        }
-
-        public Customer(int id, string name)
-            : this(id)
-        {
-            this.Name = name;
-        }
-    }
-
-    public class Order
-    {
-
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            Console.WriteLine(customer.Orders.Count);
         }
     }
 }
