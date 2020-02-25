83eb9b0dfe1c9f55bfe279322da8ad74a65132ec
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-24 21:32:31 -0500

    created Person class

--- 02-classes/Classes/Program.cs
+++ 02-classes/Classes/Program.cs
@@ -2,11 +2,20 @@
 
 namespace Classes
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
