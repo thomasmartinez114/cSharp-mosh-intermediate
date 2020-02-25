eb6d8261a822c23187aeddd9afb388f93ebe9627
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-24 22:45:10 -0500

    moved Point to antoher file

---
+++ 02-classes/Methods/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace Methods
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
--- 02-classes/Methods/Program.cs
+++ 02-classes/Methods/Program.cs
@@ -2,17 +2,6 @@
 
 namespace Methods
 {
-    public class Point
-    {
-        public int X;
-        public int Y;
-
-        public Point(int x, int y)
-        {
-            this.X = x;
-            this.Y = y;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
