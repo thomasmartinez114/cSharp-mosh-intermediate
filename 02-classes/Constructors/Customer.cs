e58b447ead8467346ca413dedf2dd25b860c6a81
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-24 22:11:21 -0500

    created constructors using ctor + tab shortcut on keyboard

---
+++ 02-classes/Constructors/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace Constructors
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
---
+++ 02-classes/Constructors/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
