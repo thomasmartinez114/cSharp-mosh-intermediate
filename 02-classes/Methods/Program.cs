3cf468662e794f433e25a390b1e901619de3bc4f
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-24 22:43:41 -0500

    init Methods.cs

---
+++ 02-classes/Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Methods
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
