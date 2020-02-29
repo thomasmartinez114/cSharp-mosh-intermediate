c9e00eaa043c3dc0030713b963d292289241b59f
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-28 22:40:20 -0500

    completed Properties

---
+++ 02-classes/Properties/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }  
+        }
+    }
+}
---
+++ 02-classes/Properties/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1982, 1, 1););
+            Console.WriteLine(person.Age);
+        }
+    }
+}
