b88c36936abce64dfd102afcb6634b925a022e8e
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-29 22:04:32 -0500

    init Inheritance

---
+++ 03-association-between-classes/Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+        
+        }
+
+        public void Duplicate()
+        {
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
