ccff58a22f2ee21bc406091ad727400fc3331a63
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-01 19:12:57 -0500

    added text class

--- 03-association-between-classes/Inheritance/Program.cs
+++ 03-association-between-classes/Inheritance/Program.cs
@@ -4,7 +4,13 @@ namespace Inheritance
 {
     public class Text : PresentationObject
     {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
 
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added a link to " + url);
+        }
     }
 
     class Program
