76b53307653c06c0888eba77bd09d8d382505344
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-02 18:24:56 -0500

    used code using Composition

--- 03-association-between-classes/Composition/Program.cs
+++ 03-association-between-classes/Composition/Program.cs
@@ -6,7 +6,14 @@ namespace Composition
     {
         static void Main(string[] args)
         {
-            
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            installer.Install();
         }
     }
 }
