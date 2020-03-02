cd98ca96494a02a56caa7f708771744412c574bf
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-02 18:14:21 -0500

    init Composition

---
+++ 03-association-between-classes/Composition/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Composition
+{
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
