0cea4e52bfd13253afc2e9b1d13273d450b37b85
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-02 18:20:21 -0500

    created Installer() in new file

---
+++ 03-association-between-classes/Composition/Installer.cs
@@ -0,0 +1,17 @@
+﻿namespace Composition
+{
+    public class Installer
+    {
+        private readonly Logger _logger;
+
+        public Installer(Logger _logger)
+        {
+            _logger = Logger;
+        }
+
+        public void Install()
+        {
+            _logger.Log("We are installing the application.");
+        }
+    }
+}
--- 03-association-between-classes/Composition/Program.cs
+++ 03-association-between-classes/Composition/Program.cs
@@ -2,7 +2,6 @@
 
 namespace Composition
 {
-
     class Program
     {
         static void Main(string[] args)
