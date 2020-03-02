72680f0218fd68f38d2fc9e6e064abfe73b629ad
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-01 19:13:57 -0500

    moved Text class to own file

--- 03-association-between-classes/Inheritance/Program.cs
+++ 03-association-between-classes/Inheritance/Program.cs
@@ -2,17 +2,6 @@
 
 namespace Inheritance
 {
-    public class Text : PresentationObject
-    {
-        public int FontSize { get; set; }
-        public string FontName { get; set; }
-
-        public void AddHyperlink(string url)
-        {
-            Console.WriteLine("We added a link to " + url);
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
