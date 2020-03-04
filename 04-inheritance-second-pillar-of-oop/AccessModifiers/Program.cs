dc099e873628b6862cb61af5d51332e3da967d7f
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-03 23:27:19 -0500

    set up code to then hide

--- 04-inheritance-second-pillar-of-oop/AccessModifiers/Program.cs
+++ 04-inheritance-second-pillar-of-oop/AccessModifiers/Program.cs
@@ -9,7 +9,16 @@ namespace AccessModifiers
 
         public void Promote()
         {
+            var rating = CalculateRating();
+            if (rating == 0)
+                Console.WriteLine("Promoted to Level 1");
+            else
+                Console.WriteLine("Promoted to Level 2");
+        }
 
+        public int CalculateRating()
+        {
+            return 0;
         }
     }
     class Program
