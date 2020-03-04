0ec6106cf49b07102171f2c3128a1ebbbc2293db
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-03 23:22:03 -0500

    begain code for Access Modifiers

---
+++ 04-inheritance-second-pillar-of-oop/AccessModifiers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+        }
+    }
+}
