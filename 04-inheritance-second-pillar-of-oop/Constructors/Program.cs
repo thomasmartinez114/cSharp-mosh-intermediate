fd0e322a23a0f94731135446417429c27ceaeefd
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-04 21:34:04 -0500

    init Vehicle

---
+++ 04-inheritance-second-pillar-of-oop/Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Constructors
+{
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle is being initiliazed.");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public Car()
+        {
+            Console.WriteLine("Car is being initiliazed.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
