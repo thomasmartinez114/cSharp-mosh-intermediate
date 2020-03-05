380e6e15895af2221a87f880166915fe2564b9c6
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-05 18:18:21 -0500

    created readonly string field

--- 04-inheritance-second-pillar-of-oop/Constructors/Vehicle.cs
+++ 04-inheritance-second-pillar-of-oop/Constructors/Vehicle.cs
@@ -4,9 +4,16 @@ namespace Constructors
 {
     public class Vehicle
     {
+        private readonly string _registrationNumber;
+
         public Vehicle()
         {
             Console.WriteLine("Vehicle is being initiliazed.");
+        }
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
         }
     }
 }
