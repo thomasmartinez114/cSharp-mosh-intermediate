e0007954b6146114274b95e611b02709bbd8eac9
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-05 19:11:59 -0500

    Text : Shape in Program

--- 04-inheritance-second-pillar-of-oop/Casting/Program.cs
+++ 04-inheritance-second-pillar-of-oop/Casting/Program.cs
@@ -2,6 +2,11 @@
 
 namespace Casting
 {
+    public class Text : Shape
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+    }
 
     class Program
     {
