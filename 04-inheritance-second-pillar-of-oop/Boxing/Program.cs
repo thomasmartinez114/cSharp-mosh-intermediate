eff5c209763065ae6b84870a33e08d6ac5d58c33
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-06 22:00:05 -0500

    casted list[1] to an int

---
+++ 04-inheritance-second-pillar-of-oop/Boxing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Mosh");
+            list.Add(DateTime.Today);
+
+            var number = (int) list[1]
+        }
+    }
+}
