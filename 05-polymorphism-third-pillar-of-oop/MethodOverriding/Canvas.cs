1781130d0dbbdef4b1fc2e78f0348b9266e72caa
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-06 23:17:09 -0500

    Added Canvas.cs

---
+++ 05-polymorphism-third-pillar-of-oop/MethodOverriding/Canvas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOverriding
+{
+    public class Canvas
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                switch (shape.Type)
+                {
+                    case ShapeType.Circle:
+                        Console.WriteLine("Draw a circle");
+                        break;
+
+                    case ShapeType.Rectangle:
+                        Console.WriteLine("Draw a rectangle");
+                        break; ;
+                }
+            }
+        }
+    }
+}
