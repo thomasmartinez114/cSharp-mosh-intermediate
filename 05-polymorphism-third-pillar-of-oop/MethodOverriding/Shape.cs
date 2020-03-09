a6d5dfbe83084526013e77ebabb142e5c3e1cae3
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-08 23:18:54 -0400

    removed the ShapeType.cs --- in Shape.cs added the classes for Circles and Rectangles that are inherited from Shape

--- 05-polymorphism-third-pillar-of-oop/MethodOverriding/Shape.cs
+++ 05-polymorphism-third-pillar-of-oop/MethodOverriding/Shape.cs
@@ -4,11 +4,20 @@ using System.Text;
 
 namespace MethodOverriding
 {
+    public class Circle : Shape
+    {
+
+    }
+
+    public class Rectangle : Shape
+    {
+
+    }
+
     public class Shape
     {
         public int Width { get; set; }
         public int Height { get; set; }
         public Position Position { get; set; }
-        public ShapeType Type { get; set; }
     }
 }
