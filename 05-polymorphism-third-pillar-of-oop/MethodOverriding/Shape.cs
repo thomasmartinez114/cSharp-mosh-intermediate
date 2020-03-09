d2aa958c04019698f7d6b04769d26145cb58fc50
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-08 22:38:46 -0400

    added Shape.cs class

---
+++ 05-polymorphism-third-pillar-of-oop/MethodOverriding/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOverriding
+{
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
+        public ShapeType Type { get; set; }
+    }
+}
