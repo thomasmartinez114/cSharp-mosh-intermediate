1254512f506912a3c1327f1efb9fb650dad9b9d6
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-10 01:37:44 -0400

    added triangle

--- 05-polymorphism-third-pillar-of-oop/MethodOverriding/Program.cs
+++ 05-polymorphism-third-pillar-of-oop/MethodOverriding/Program.cs
@@ -7,8 +7,8 @@ namespace MethodOverriding
         static void Main(string[] args)
         {
             var shapes = new List<Shape>();
-            shapes.Add(new Shape { Width = 100, Height = 100, Type = ShapeType.Circle });
-            shapes.Add(new Shape { Width = 100, Height = 30, Type = ShapeType.Rectangle });
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
 
             var canvas = new Canvas();
             canvas.DrawShapes(shapes);
--- 05-polymorphism-third-pillar-of-oop/MethodOverriding/Shape.cs
+++ 05-polymorphism-third-pillar-of-oop/MethodOverriding/Shape.cs
@@ -21,6 +21,14 @@ namespace MethodOverriding
         }
     }
 
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a triangle");
+        }
+    }
+
     public class Shape
     {
         public int Width { get; set; }
