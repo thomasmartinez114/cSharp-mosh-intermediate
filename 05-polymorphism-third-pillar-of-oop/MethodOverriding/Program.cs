65b6a1eb4d167d614804b9e517e2e016968173ab
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-10 01:11:20 -0400

    added code to Program.cs

--- 05-polymorphism-third-pillar-of-oop/MethodOverriding/Program.cs
+++ 05-polymorphism-third-pillar-of-oop/MethodOverriding/Program.cs
@@ -2,11 +2,16 @@
 
 namespace MethodOverriding
 {
-    public class Shape
+    class Program
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public Position Position { get; set; }
-        public ShapeType Type { get; set; }
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>();
+            shapes.Add(new Shape { Width = 100, Height = 100, Type = ShapeType.Circle });
+            shapes.Add(new Shape { Width = 100, Height = 30, Type = ShapeType.Rectangle });
+
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
+        }
     }
 }
