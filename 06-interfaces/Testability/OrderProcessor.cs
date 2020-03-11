3db25537a2d509bd50f0058dd598dcf7575d1f8b
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-10 20:08:35 -0400

    added OrderProcessor.js

---
+++ 06-interfaces/Testability/OrderProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testability
+{
+    class OrderProcessor
+    {
+        private readonly ShippingCalculator _shippingCalculator;
+
+        public OrderProcessor()
+        {
+            _shippingCalculator = new ShippingCalculator();
+        }
+
+        public void Process(OrderProcessor order)
+        {
+            if (order.IsShipped)
+                throw new InvalidOperationException("This order is already processed.");
+
+            order.Shipment = new Shipment
+            {
+                Cost = _shippingCalculator.CalculateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1)
+            };
+        }
+    }
+}
